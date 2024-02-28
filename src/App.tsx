79021da6c317fc1c2caa75e8b96776b0567976df
livehass <silva.felipe12@hotmail.com>
2024-02-28 17:54:59 -0300

    cria crud categoria

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
+import Navbar from './components/navbar/Navbar'
+import Home from './pages/home/Home';
+import Footer from './components/footer/Footer'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import FormularioCategoria from './components/categorias/formularioCategoria/FormularioCategoria';
+import ListaCategorias from './components/categorias/listaCategorias/ListaCategorias';
+import RemoverCategoria from './components/categorias/removerCategoria/RemoverCategoria';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ToastContainer />
+      <BrowserRouter>
+        <Navbar />
+        <div className='min-h-[80vh] width-100 bg-gradient-to-r from-blue-500 to-blue-600'>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/categorias" element={<ListaCategorias />} />
+            <Route path="/cadastroCategoria" element={<FormularioCategoria />} />
+            <Route path="/editarCategoria/:id" element={<FormularioCategoria/>} />
+            <Route path="/removerCategoria/:id" element={<RemoverCategoria />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
